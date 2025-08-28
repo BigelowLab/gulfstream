ec889f24c68c3b432bd4c11e26ce86c4e3ed9547
Ben Tupper <btupper@bigelow.org>
2025-08-28 12:01:05 -0400

    auto update 2025-08-28 12:01:05

--- inst/scripts/build_and_push.R
+++ inst/scripts/build_and_push.R
@@ -4,6 +4,9 @@ devtools::install(path, upgrade = FALSE)
 
 orig = setwd(path)
 
+# add
+ok = system("git add *")
+
 # commit
 date = format(Sys.time(), "%Y-%m-%d %H:%M:%S")
 msg = sprintf("git commit -a -m 'auto update %s'", date)
