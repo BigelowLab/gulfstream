3eb7d4af1b8eafe1b49c74d93c7ea0993219fb75
Ben Tupper <btupper@bigelow.org>
2025-08-28 11:09:47 -0400

    auto update 2025-08-28 11:09:47

--- inst/scripts/build_and_push.R
+++ inst/scripts/build_and_push.R
@@ -2,9 +2,13 @@ path = gulfstream::gulfstream_path()
 devtools::install(path, upgrade = FALSE)
 
 orig = setwd(path)
+
+# commit
 date = format(Sys.time(), "%Y-%m-%d %H:%M:%S")
 msg = sprintf("git commit -a -m 'auto update %s'", date)
 ok = system(msg)
+
+# now push
 ok = system("git push origin main")
 setwd(orig)
 
