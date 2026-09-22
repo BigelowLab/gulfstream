77f0fbd30ff343d77d3dcde01f564639e3ca039a
Ben Tupper <btupper@bigelow.org>
2026-09-22 11:38:41 -0400

    auto update 2026-09-22 11:38:41

--- inst/scripts/build_and_push.R
+++ inst/scripts/build_and_push.R
@@ -1,49 +1,44 @@
-suppressPackageStartupMessages({
-  library(gstream)
-  library(gulfstream)
-})
-
 
 remotes::install_github("NOAA-EDAB/ecodata", upgrade = FALSE)
 
 # here we update copies of the datasets (so we have the freshest)
 
 # USN gulf stream data 
-ofile = gulfstream_path("inst/extdata/usn.rds")
+ofile = gulfstream::gulfstream_path("inst/extdata/usn.rds")
 x = gstream::read_usn(what = "ordered", deduplicate = TRUE) |>
   saveRDS(ofile)
 
 # GSI
-ofile = gulfstream_path("inst/extdata/gsi.rds")
+ofile = gulfstream::gulfstream_path("inst/extdata/gsi.rds")
 x = gstream::read_gsi() |>
   saveRDS(file = ofile)
 
 # GSGI
-ofile = gulfstream_path("inst/extdata/gsgi.rds")
+ofile = gulfstream::gulfstream_path("inst/extdata/gsgi.rds")
 x = gstream::read_gsgi() |>
   saveRDS(ofile)
 
 # rapid-mocha
-ofile = gulfstream_path("inst/extdata/rapid_mocha.rds")
+ofile = gulfstream::gulfstream_path("inst/extdata/rapid_mocha.rds")
 x = gstream::read_rapid_mocha() |>
   saveRDS(ofile)
 
 # transport-mocha
-ofile = gulfstream_path("inst/extdata/moc-transports.rds")
+ofile = gulfstream::gulfstream_path("inst/extdata/moc-transports.rds")
 x = gstream::read_moc_transports() |>
   saveRDS(ofile)
 
 # patches
-ofile = gulfstream_path("inst/extdata/patch_bbs.rds")
+ofile = gulfstream::gulfstream_path("inst/extdata/patch_bbs.rds")
 x = gstream::read_patch_bbs() |>
   saveRDS(ofile)
 
-ofile = gulfstream_path("inst/extdata/patch_month.rds")
+ofile = gulfstream::gulfstream_path("inst/extdata/patch_month.rds")
 x = gstream::read_patch_month() |>
   saveRDS(ofile)
 
 # coastline
-ofile = gulfstream_path("inst/extdata/coast_medium.rds")
+ofile = gulfstream::gulfstream_path("inst/extdata/coast_medium.rds")
 coast = rnaturalearth::ne_coastline(returnclass = "sf", scale = "medium") |>
   sf::st_geometry() |>
   sf::st_simplify()
